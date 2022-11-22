7c7a18624fd1856cd8f35c40642019cdd1701dd0
DanielWretsen <danielwret@gmail.com>
2022-11-22 16:51:23 +0100

    test

--- src/Ovning2_18.java
+++ src/Ovning2_18.java
@@ -1,28 +1,27 @@
-
+// Importera funktionalitet för att läsa in data från tangentbord
 import java.util.Scanner;
 
-        public class Ovning2_18 {
+public class Ovning2_18
+{
 
-            public static void main(String[] args) {
-                // Skapa inläsningsmöjlighet från datorns tangentbord
-                Scanner input = new Scanner( System.in );
+    public static void main(String[] args)
+    {
+        // Skapa inläsningsmöjlighet från datorns tangentbord
+        Scanner input = new Scanner( System.in );
 
-                System.out.println("Ange priset exklusive moms: ");
-                int priceWithoutTax = input.nextInt();
+        System.out.println("Ange priset exklusive moms: ");
+        double priceWithoutTax = input.nextDouble();
 
-                System.out.println("Ange aktuell momssats som procent (0, 6, 12, 25): ");
-                int taxRateInPercentage = input.nextInt();
+        System.out.println("Ange aktuell momssats som procent (0, 6, 12, 25): ");
+        int taxRateInPercentage = input.nextInt();
 
-                int tax = priceWithoutTax * taxRateInPercentage / 100;
+        double tax = (double) priceWithoutTax * (double) taxRateInPercentage / 100;
 
-                System.out.println( "Vid priset " + priceWithoutTax + " SEK och momssatsen " + taxRateInPercentage + "%");
-                System.out.println( "blir mervärdesskatten: " + tax + " SEK");
+        System.out.println( "Vid priset " + priceWithoutTax + " SEK och momssatsen " + taxRateInPercentage + "%");
+        System.out.println( "blir mervärdesskatten: " + tax + " SEK");
 
-                // Stäng resursen som läser in data från tangentbordet
-                input.close();
-
-            }
-        }
+        // Stäng resursen som läser in data från tangentbordet
+        input.close();
 
     }
 }
